12fa6acb5ed0f625db9ed4a586f7df3f1a370162
DahiseDias <dahisedias@gmail.com>
2025-06-02 21:35:36 -0300

    mostrando imagem real

--- myFetus/app/gestation-info.tsx
+++ myFetus/app/gestation-info.tsx
@@ -16,6 +16,53 @@ import { calculateGestationWeek } from '../utils/gestationUtils';
 
 const { width, height } = Dimensions.get('window');
 
+// Mapeamento das imagens das semanas
+const fetusImages: { [key: string]: any } = {
+  '04': require('../assets/images/Fetus_weeks_img/04-fetaldev-E-deeptan_4x3.jpg'),
+  '05': require('../assets/images/Fetus_weeks_img/05-fetaldev-E-deeptan_4x3.jpg'),
+  '06': require('../assets/images/Fetus_weeks_img/06-fetaldev-E-deeptan_4x3.jpg'),
+  '07': require('../assets/images/Fetus_weeks_img/07-fetaldev-E-deeptan_4x3.jpg'),
+  '08': require('../assets/images/Fetus_weeks_img/08-fetaldev-E-deeptan_4x3.jpg'),
+  '09': require('../assets/images/Fetus_weeks_img/09-fetaldev-E-deeptan_4x3.jpg'),
+  '10': require('../assets/images/Fetus_weeks_img/10-fetaldev-E-deeptan_4x3.jpg'),
+  '11': require('../assets/images/Fetus_weeks_img/11-fetaldev-E-deeptan_4x3.jpg'),
+  '12': require('../assets/images/Fetus_weeks_img/12-fetaldev-E-deeptan_4x3.jpg'),
+  '13': require('../assets/images/Fetus_weeks_img/13-fetaldev-E-deeptan_4x3.jpg'),
+  '14': require('../assets/images/Fetus_weeks_img/14-fetaldev-E-deeptan_4x3.jpg'),
+  '15': require('../assets/images/Fetus_weeks_img/15-fetaldev-E-deeptan_4x3.jpg'),
+  '16': require('../assets/images/Fetus_weeks_img/16-fetaldev-E-deeptan_4x3.jpg'),
+  '17': require('../assets/images/Fetus_weeks_img/17-fetaldev-E-deeptan_4x3.jpg'),
+  '18': require('../assets/images/Fetus_weeks_img/18-fetaldev-E-deeptan_4x3.jpg'),
+  '19': require('../assets/images/Fetus_weeks_img/19-fetaldev-E-deeptan_4x3.jpg'),
+  '20': require('../assets/images/Fetus_weeks_img/20-fetaldev-E-deeptan_4x3.jpg'),
+  '21': require('../assets/images/Fetus_weeks_img/21-fetaldev-E-deeptan_4x3.jpg'),
+  '22': require('../assets/images/Fetus_weeks_img/22-fetaldev-E-deeptan_4x3.jpg'),
+  '23': require('../assets/images/Fetus_weeks_img/23-fetaldev-E-deeptan_4x3.jpg'),
+  '24': require('../assets/images/Fetus_weeks_img/24-fetaldev-E-deeptan_4x3.jpg'),
+  '25': require('../assets/images/Fetus_weeks_img/25-fetaldev-E-deeptan_4x3.jpg'),
+  '26': require('../assets/images/Fetus_weeks_img/26-fetaldev-E-deeptan_4x3.jpg'),
+  '27': require('../assets/images/Fetus_weeks_img/27-fetaldev-E-deeptan_4x3.jpg'),
+  '28': require('../assets/images/Fetus_weeks_img/28-fetaldev-E-deeptan_4x3.jpg'),
+  '29': require('../assets/images/Fetus_weeks_img/29-fetaldev-E-deeptan_4x3.jpg'),
+  '30': require('../assets/images/Fetus_weeks_img/30-fetaldev-E-deeptan_4x3.jpg'),
+  '31': require('../assets/images/Fetus_weeks_img/31-fetaldev-E-deeptan_4x3.jpg'),
+  '32': require('../assets/images/Fetus_weeks_img/32-fetaldev-E-deeptan_4x3.jpg'),
+  '33': require('../assets/images/Fetus_weeks_img/33-fetaldev-E-deeptan_4x3.jpg'),
+  '34': require('../assets/images/Fetus_weeks_img/34-fetaldev-E-deeptan_4x3.jpg'),
+  '35': require('../assets/images/Fetus_weeks_img/35-fetaldev-E-deeptan_4x3.jpg'),
+  '36': require('../assets/images/Fetus_weeks_img/36-fetaldev-E-deeptan_4x3.jpg'),
+  '37': require('../assets/images/Fetus_weeks_img/37-fetaldev-E-deeptan_4x3.jpg'),
+  '38': require('../assets/images/Fetus_weeks_img/38-fetaldev-E-deeptan_4x3.jpg'),
+  '39': require('../assets/images/Fetus_weeks_img/39-fetaldev-E-deeptan_4x3.jpg'),
+  '40': require('../assets/images/Fetus_weeks_img/40-fetaldev-E-deeptan_4x3.jpg'),
+  '41': require('../assets/images/Fetus_weeks_img/41-fetaldev-E-deeptan_4x3.jpg'),
+};
+
+const getFetusImage = (week: number) => {
+  const formattedWeek = week.toString().padStart(2, '0');
+  return fetusImages[formattedWeek] || require('../assets/images/fetus-heart.png');
+};
+
 export default function GestationInfoScreen() {
   const router = useRouter();
   const params = useLocalSearchParams();
@@ -209,9 +256,9 @@ export default function GestationInfoScreen() {
             </Svg>
           </Animated.View>
           <Image
-            source={require('../assets/images/fetus-heart.png')}
+            source={getFetusImage(gestationWeeks)}
             style={styles.fetusImage}
-            resizeMode="contain"
+            resizeMode="cover"
           />
         </View>
 
@@ -257,12 +304,13 @@ const styles = StyleSheet.create({
     fontWeight: '500',
   },
   imageContainer: {
-    width: width * 0.8,
-    height: width * 0.8,
+    width: width * 0.7,
+    height: width * 0.7,
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: height * 0.04,
     position: 'relative',
+    
   },
   wave: {
     position: 'absolute',
@@ -270,11 +318,25 @@ const styles = StyleSheet.create({
     height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    marginTop: -height * 0.02,
   },
   fetusImage: {
-    width: '100%',
-    height: '100%',
+    width: width * 0.6,
+    height: width * 0.6,
+    borderRadius: width * 0.3,
+    backgroundColor: '#fff',
     zIndex: 1,
+    overflow: 'hidden',
+    transform: [{ scale: 1.1 }],
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginLeft: -(width * 0.3),
+    marginTop: -(width * 0.3),
   },
   description: {
     fontSize: width * 0.045,
