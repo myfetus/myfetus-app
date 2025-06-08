922a66a320d9c849c928cf8761ddb00206fe1011
alocinny <alocinnygame@gmail.com>
2025-06-07 21:02:47 -0300

    MYF-53 criptografar a senha

--- myFetus/backend/controllers/userController.js
+++ myFetus/backend/controllers/userController.js
@@ -1,14 +1,18 @@
 const client = require('../backend');
 const updateEntity = require('../utils/updateEntity');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 10
 
 // TODO: validação
 
 const createUser = async (req, res) => {
   const { name, email, password, birthdate, is_active = true, role = 'user' } = req.body;
   try {
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
     const result = await client.query(
       'INSERT INTO users (name, email, password, birthdate, is_active, role) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-      [name, email, password, birthdate, is_active, role]
+      [name, email, hashedPassword, birthdate, is_active, role]
     );
     res.status(201).json(result.rows[0]);
   } catch (err) {
@@ -37,6 +41,14 @@ const getUserById = async (req, res) => {
 
 const updateUser = async (req, res) => {
   try {
+    const { password, ...rest } = req.body;
+    let updateData = rest;
+
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+      updateData.password = hashedPassword;
+    }
+
     const updatedUser = await updateEntity('users', req.params.id, req.body);
     if (!updatedUser) return res.status(404).send('Usuário não encontrado');
     res.json(updatedUser);
@@ -55,10 +67,37 @@ const deleteUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await client.query('SELECT * FROM users WHERE email = $1', [email]);
+
+    const user = result.rows[0];
+
+    if (result.rows.length === 0 || !(await bcrypt.compare(password, result.rows[0].password))) {
+      return res.status(401).json({ error: 'Login ou senha inválidos'});
+    }
+
+    res.status(200).json({
+      message: 'Login bem-sucedido',
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ error: 'Erro no servidor' });
+  }
+};
+
 module.exports = {
   createUser,
   getUsers,
   getUserById,
   updateUser,
   deleteUser,
+  loginUser,
 };
